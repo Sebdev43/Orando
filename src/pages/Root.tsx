557dca7f7434b4e7b99e55271358f43968905d3a
Heike13 <matthias.hibon@oclock.school>
2024-06-26 19:42:21 +0200

    test : menu for demo

--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import Nav from '../components/Nav/Nav';
 
 function Root() {
   return (
     <div className="app">
+      <Nav />
       <Outlet />
     </div>
   );
