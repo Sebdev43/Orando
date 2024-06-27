562dad6fb78dc1353bac9e2659f71a1ed508c3ea
KevinCannizzaro <skylar74@outlook.fr>
2024-06-27 10:57:51 +0200

    reset main branch

--- migrations/deploy/00-pluginPostgis.sql
+++
@@ -1,8 +0,0 @@
--- Deploy orando:00-pluginPostgis to pg
-
-BEGIN;
-
-CREATE EXTENSION postgis;
-CREATE EXTENSION postgis_topology; --optionnel pour des fonctionnalité complementaire
-
-COMMIT;
--- migrations/revert/01-insertTables.sql
+++
@@ -1,9 +0,0 @@
--- Revert orando:01-insertTables from pg
-
-BEGIN;
-
-DROP TABLE IF EXISTS "users_has_hikes","users","hikes";
-
-DROP DOMAIN IF EXISTS "email";
-
-COMMIT;
