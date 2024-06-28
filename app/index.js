e0eea53441787e7cb788d191286f7a9c50383c10
Sebdev43 <sebastien.robert@oclock.school>
2024-06-28 14:54:30 +0200

    feat: :white_check_mark: Ajout du serveur central et de de l'installation de swagger( A partir d'ici pensée à mettre la Jcdoc)

---
+++ app/index.js
@@ -0,0 +1,13 @@
+//* Importation des modules
+
+//import usersRoutes from './routers/userRoutes.js';
+//import hikesRoutes from './routers/hikeRoutes.js';
+
+
+
+export function initializeRoutes(app){
+    //app.use('/hikes', hikesRoutes);
+    //app.use('/users', usersRoutes);
+
+}
+
--- server.js
+++ server.js
@@ -1,1 +1,47 @@
-/* test */
+/* Point d'entrée de l'appliaction*/
+
+//* Importation des modules
+import express from 'express';
+//import swaggerJSDoc from 'swagger-jsdoc';
+//import swaggerUi from 'swagger-ui-express';
+import {initializeRoutes} from './app/index.js';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//* Configuration de Swagger
+
+const swaggerOptions = {
+    openapi:'3.0.0',
+    info: {
+        title: 'API de O\'rando',
+        version: '1.0.0',
+        description: 'Ce projet vise à développer un site internet de randonnées, permettant aux utilisateurs de découvrir et de partager des itinéraires de randonnées. La partie backend de ce projet utilise Node.js pour le serveur et PostgreSQL comme base de données. Nous avons mis en place une API RESTful pour gérer les opérations CRUD (Create, Read, Update, Delete) sur les données de randonnées.',
+    
+    },
+    servers: [
+        {
+            url: 'http://localhost:3000'
+        }
+    ],
+    apis: ['./app/routes/*.js', './app/controllers/*.js','.app/datamappers/*.js'],//chemin vers les fichiers contenant les annotations swagger
+};
+
+// Initialisation de Swagger-jsdoc
+
+//const swaggerDocs = swaggerJSDoc(swaggerOptions);
+//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Middleware pour parser le JSON
+
+app.use(express.json());
+
+// Initialisation des routes
+
+initializeRoutes(app);
+
+// Démarrage du serveur
+
+app.listen(port, () => {
+    console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
+});
