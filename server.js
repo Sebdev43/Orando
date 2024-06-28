5b36bdeae70028b5e42f59a00aa29aff7be70c18
Sebdev43 <sebastien.robert@oclock.school>
2024-06-28 16:24:11 +0200

    refactor: :lipstick: Transformation de l'implementation swagger en middleware

--- server.js
+++ server.js
@@ -1,40 +1,19 @@
 /* Point d'entrée de l'appliaction*/
 
 //* Importation des modules
-import express from 'express';
-//import swaggerJSDoc from 'swagger-jsdoc';
-//import swaggerUi from 'swagger-ui-express';
-import {initializeRoutes} from './app/index.js';
+import express from "express";
+import { initializeRoutes, initializeSwagger } from "./app/index.app.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-//* Configuration de Swagger
-
-const swaggerOptions = {
-    openapi:'3.0.0',
-    info: {
-        title: 'API de O\'rando',
-        version: '1.0.0',
-        description: 'Ce projet vise à développer un site internet de randonnées, permettant aux utilisateurs de découvrir et de partager des itinéraires de randonnées. La partie backend de ce projet utilise Node.js pour le serveur et PostgreSQL comme base de données. Nous avons mis en place une API RESTful pour gérer les opérations CRUD (Create, Read, Update, Delete) sur les données de randonnées.',
-    
-    },
-    servers: [
-        {
-            url: 'http://localhost:3000'
-        }
-    ],
-    apis: ['./app/routes/*.js', './app/controllers/*.js','.app/datamappers/*.js'],//chemin vers les fichiers contenant les annotations swagger
-};
-
-// Initialisation de Swagger-jsdoc
-
-//const swaggerDocs = swaggerJSDoc(swaggerOptions);
-//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
 // Middleware pour parser le JSON
 
 app.use(express.json());
+
+//Initialiser Swagger
+
+initializeSwagger(app);
 
 // Initialisation des routes
 
@@ -43,5 +22,5 @@ initializeRoutes(app);
 // Démarrage du serveur
 
 app.listen(port, () => {
-    console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
+  console.log(`Serveur en cours d'exécution sur http://localhost:${port}`);
 });
