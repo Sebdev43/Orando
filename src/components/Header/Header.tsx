472c7c7dd06bffd1ceaf379915cb10b7c8d2503e
Heike13 <matthias.hibon@oclock.school>
2024-06-28 18:59:46 +0200

    feat: breadcrumbs / fully loaded

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,14 @@
 import './styles.scss';
 import Nav from '../Nav/Nav';
 import { NavLink } from 'react-router-dom';
+import IconBreadcrumbs from '../Breadcrumbs/Breadcrumbs';
 
 function Header() {
   return (
     <div className="header">
+      <NavLink className="header-top-button" to={'/connexion'}>
+        Se connecter
+      </NavLink>
       <NavLink to={'/'}>
         <img
           src="./src/assets/logo_fond_blanc.png"
@@ -12,11 +16,9 @@ function Header() {
           className="header-top-logo"
         />
       </NavLink>
-      <NavLink className="header-top-button" to={'/connexion'}>
-        Se connecter
-      </NavLink>
 
       <Nav />
+      <IconBreadcrumbs />
     </div>
   );
 }
--- src/pages/Root.tsx
+++ src/pages/Root.tsx
@@ -2,12 +2,14 @@ import { Outlet } from 'react-router-dom';
 
 import './Root.scss';
 import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
 
 function Root() {
   return (
     <div className="app">
       <Header />
       <Outlet />
+      <Footer />
     </div>
   );
 }
