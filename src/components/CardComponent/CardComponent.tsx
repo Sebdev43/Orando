7877524bfe02a29cc67a8fc7cc9f22532df4cbc2
Heike13 <matthias.hibon@oclock.school>
2024-07-02 02:12:17 +0200

    feat: add colors on every difficulties

--- src/components/CardComponent/CardComponent.tsx
+++ src/components/CardComponent/CardComponent.tsx
@@ -9,13 +9,40 @@ import Divider from '@mui/joy/Divider';
 import Typography from '@mui/joy/Typography';
 import IconButton from '@mui/joy/IconButton';
 import Favorite from '@mui/icons-material/Favorite';
+// import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
 
 import './CardComponent.scss';
 
 function CardComponent(hike: Hike) {
+  // Put some color depends of the difficulty
+  function renderDificulty() {
+    switch (hike.difficulty) {
+      case 'Facile':
+        return (
+          <Typography color="success" level="body-sm">
+            {hike.difficulty}
+          </Typography>
+        );
+      case 'Moyenne':
+        return (
+          <Typography color="warning" level="body-sm">
+            {hike.difficulty}
+          </Typography>
+        );
+      case 'Difficile':
+        return (
+          <Typography color="danger" level="body-sm">
+            {hike.difficulty}
+          </Typography>
+        );
+      default:
+        return <Typography level="body-sm">{hike.difficulty}</Typography>;
+    }
+  }
+
   return (
     <NavLink to={`/randonnees/${hike.slug}`} className="no-decoration">
-      <Card variant="outlined" sx={{ width: 340 }}>
+      <Card variant="outlined" sx={{ width: 360, height: 500 }}>
         <CardOverflow>
           {/* Image */}
           <AspectRatio ratio="1">
@@ -30,34 +57,36 @@ function CardComponent(hike: Hike) {
           <IconButton
             aria-label="Like minimal photography"
             size="sm"
-            variant="solid"
-            color="danger"
+            variant="plain"
             sx={{
               position: 'absolute',
               zIndex: 2,
               borderRadius: '50%',
               right: '0.5rem',
-              bottom: 24,
+              bottom: 330,
               transform: 'translateY(50%)',
+              color: 'red',
+              '&:hover': { color: '#da7b29' },
             }}
           >
+            {/*@TODO Add to bookmarks : if already in bookmarks, change to Favorite icons */}
             <Favorite />
           </IconButton>
         </CardOverflow>
         {/* Card content */}
         <CardContent>
-          <h3>{hike.title}</h3>
+          <h3 className="card__title">{hike.title}</h3>
         </CardContent>
         {/* Card footer */}
         <CardOverflow variant="soft">
           <Divider inset="context" />
           {/* Card tags */}
           <CardContent orientation="horizontal">
-            <Typography level="body-xs">{hike.localisation}</Typography>
+            <Typography level="body-sm">{hike.localisation}</Typography>
             <Divider orientation="vertical" />
-            <Typography level="body-xs">{hike.difficulty}</Typography>
+            <Typography level="body-sm">{hike.distance} km</Typography>
             <Divider orientation="vertical" />
-            <Typography level="body-xs">Environ {hike.distance} km</Typography>
+            {renderDificulty()}
           </CardContent>
         </CardOverflow>
       </Card>
