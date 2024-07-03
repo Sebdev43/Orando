06fbb1f7ac2f65769ac133afdfc93f933c7a7300
KevinCannizzaro <skylar74@outlook.fr>
2024-07-04 01:17:06 +0200

    JsDoc Swagger for all methode user

--- app/routers/userRouter.js
+++ app/routers/userRouter.js
@@ -1,15 +1,7 @@
 import express from "express";
-import {
-  createUser,
-  updateUser,
-  getUserById,
-} from "../controllers/userController.js";
+import { createUser, updateUser, getUserById, deleteUser} from "../controllers/userController.js";
 import { hashPasswordMiddleware } from "../middlewares/scryptMiddleware.js";
-import {
-  emailValidator,
-  nicknameValidator,
-  passwordValidator,
-} from "../validators/userValidators.js";
+import { emailValidator, nicknameValidator, passwordValidator} from "../validators/userValidators.js";
 
 import { validateRequest } from "../middlewares/validateReqMiddleware.js";
 
@@ -19,14 +11,15 @@ const router = express.Router();
  * @swagger
  * tags:
  *   - name: Users
- *     description: Créations des données utilisateurs
+ *     description: Operations related to users
  */
+
 /**
  * @swagger
  * /users:
  *   post:
- *     summary: Créer un nouvel utilisateur
- *     description: Cette endpoint permet de créer un nouvel utilisateur.
+ *     summary: Create a new user
+ *     description: Endpoint to create a new user
  *     tags: [Users]
  *     requestBody:
  *       required: true
@@ -35,141 +28,116 @@ const router = express.Router();
  *           schema:
  *             type: object
  *             properties:
- *              nickname:
- *                type: string
- *              localisation:
- *                type: string
- *              email:
- *                type: string
- *              password:
- *                type: string
+ *               nickname:
+ *                 type: string
+ *               localisation:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
  *     responses:
  *       '201':
- *         description: Utilisateur créé avec succès
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   id:
- *                     type: integer
- *                     description: ID de l'utilisateur
- *                   nickname:
- *                     type: string
- *                     description: Pseudo de l'utilisateur
- *                   localisation:
- *                     type: string
- *                     description: Département de l'utilisateur
- *                   email:
- *                     type: string
- *                     format: email
- *                     description: Email de l'utilisateur
- *                   password:
- *                     type: string
- *                     description: Mot de passe de l'utilisateur
- *                   created_at:
- *                     type: string
- *                     format: date-time
- *                     description: Date de création de l'utilisateur
- *                   updated_at:
- *                     type: string
- *                     format: date-time
- *                     description: Date de mise à jour de l'utilisateur
+ *         description: User created successfully
  *       '400':
- *         description: Erreur lors de la création de l'utilisateur
+ *         description: Error creating user
  *       '500':
- *         description: Erreur interne avec le serveur
- *
+ *         description: Internal server error
  */
-router.post(
-  "/",
-  [emailValidator, passwordValidator, nicknameValidator],
+router.post("/",[emailValidator, passwordValidator, nicknameValidator],
   validateRequest,
   hashPasswordMiddleware,
   createUser
 );
+  
+  /**
+   * @swagger
+   * /users/{id}:
+   *   get:
+   *     summary: Get user by ID
+   *     description: Retrieve user details by their ID
+   *     tags: [Users]
+   *     parameters:
+   *       - name: id
+   *         in: path
+   *         required: true
+   *         description: ID of the user to retrieve
+   *         schema:
+   *           type: integer
+   *     responses:
+   *       '200':
+   *         description: User found
+   *       '404':
+   *         description: User not found
+   *       '500':
+   *         description: Internal server error
+   */
+  router.get("/:id", getUserById);
 
-/**
- * @swagger
- * /users/{id}:
- *   patch:
- *     summary: Modifier un utilisateur
- *     description: Cette endpoint permet de modifier un utilisateur.
- *     tags: [Users]
- *     parameters:
- *         -in: path
- *         name: id
- *         schema:
- *          type: integer
- *         required: true
- *         description: Id de l'utilisateur
- *     requestBody:
- *         required: true
- *         content:
- *           application/json:
- *            schema:
- *             type: object
- *             properties:
- *              nickname:
- *                type: string
- *              localisation:
- *                type: string
- *              email:
- *                type: string
- *              password:
- *                type: string
- *     responses:
- *       '201':
- *         description: Les données utilisateur on été modifié avec succès
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   id:
- *                     type: integer
- *                     description: ID de l'utilisateur
- *                   nickname:
- *                     type: string
- *                     description: Pseudo de l'utilisateur
- *                   localisation:
- *                     type: string
- *                     description: Département de l'utilisateur
- *                   email:
- *                     type: string
- *                     format: email
- *                     description: Email de l'utilisateur
- *                   password:
- *                     type: string
- *                     description: Mot de passe de l'utilisateur
- *                   created_at:
- *                     type: string
- *                     format: date-time
- *                     description: Date de création de l'utilisateur
- *                   updated_at:
- *                     type: string
- *                     format: date-time
- *                     description: Date de mise à jour de l'utilisateur
- *       '400':
- *         description: Erreur lors de la création de l'utilisateur
- *       '500':
- *         description: Erreur interne avec le serveur
- *
- */
-router.patch(
-  "/:id",
-  [emailValidator, passwordValidator, nicknameValidator],
-  validateRequest,
-  passwordValidator,
-  updateUser
-);
-
-router.get("/:id", getUserById);
-
-router.delete("/:id", deleteUser);
+  /**
+   * @swagger
+   * /users/{id}:
+   *   patch:
+   *     summary: Update user by ID
+   *     description: Update user information by their ID
+   *     tags: [Users]
+   *     parameters:
+   *       - name: id
+   *         in: path
+   *         required: true
+   *         description: ID of the user to update
+   *         schema:
+   *           type: integer
+   *     requestBody:
+   *       required: true
+   *       content:
+   *         application/json:
+   *           schema:
+   *             type: object
+   *             properties:
+   *               nickname:
+   *                 type: string
+   *               localisation:
+   *                 type: string
+   *               email:
+   *                 type: string
+   *               password:
+   *                 type: string
+   *     responses:
+   *       '200':
+   *         description: User updated successfully
+   *       '400':
+   *         description: Error updating user
+   *       '404':
+   *         description: User not found
+   *       '500':
+   *         description: Internal server error
+   */
+  router.patch("/:id",[emailValidator, passwordValidator, nicknameValidator], validateRequest, passwordValidator, updateUser);
+  
+  /**
+   * @swagger
+   * /users/{id}:
+   *   delete:
+   *     summary: Delete user by ID
+   *     description: Delete user by their ID
+   *     tags: [Users]
+   *     parameters:
+   *       - name: id
+   *         in: path
+   *         required: true
+   *         description: ID of the user to delete
+   *         schema:
+   *           type: integer
+   *     responses:
+   *       '200':
+   *         description: User deleted successfully
+   *       '404':
+   *         description: User not found
+   *       '500':
+   *         description: Internal server error
+   */
+  router.delete("/:id", deleteUser);
+  
 
 export default router;
