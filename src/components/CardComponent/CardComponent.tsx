1f491614ffa3307181dfd0018f3dfa824cae864d
Heike13 <matthias.hibon@oclock.school>
2024-07-04 14:37:02 +0200

    techk some details / fix some bug

--- src/components/CardComponent/CardComponent.tsx
+++ src/components/CardComponent/CardComponent.tsx
@@ -27,7 +27,7 @@ function CardComponent(hike: Hike) {
       <CardOverflow>
         {/* Photo */}
         <AspectRatio ratio="1">
-          <img src={hike.pictures[1]} loading="lazy" alt="" />
+          <img src={hike.pictures[0]} loading="lazy" alt="" />
         </AspectRatio>
         {/* Booksmarks button */}
         {/* ligne 64 : la fonction qui va déclencher la logique liée à l'add/del d'un favori  */}
