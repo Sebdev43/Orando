be4cf3a239b35cff96c07edf633b84e52380b767
Sebdev43 <sebastien.robert@oclock.school>
2024-07-11 10:26:15 +0200

    refactor: :lipstick: Add Constraint users_has_hikes to docker

--- data/01-insertTables.sql
+++ data/01-insertTables.sql
@@ -38,3 +38,5 @@ CREATE TABLE "users_has_hikes" (
     "created_at" TIMESTAMPTZ NOT NULL DEFAULT now(),
     "updated_at" TIMESTAMPTZ
 );
+
+ALTER TABLE "users_has_hikes" ADD CONSTRAINT unique_user_hike UNIQUE ("users_id", "hikes_id");
