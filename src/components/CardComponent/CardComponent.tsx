c35998d51ae4497cd74f576656b192c9303db95c
Heike13 <matthias.hibon@oclock.school>
2024-07-17 19:45:25 +0200

    no

--- src/components/CardComponent/CardComponent.tsx
+++ src/components/CardComponent/CardComponent.tsx
@@ -29,8 +29,8 @@ export default function CardComponent(hike: Hike) {
           sx={{ width: 400, height: 640 }}
         >
           <CardOverflow>
-             {/* Bookmark component */}
-          <Bookmarks id={hike.id} />
+            {/* Bookmark component */}
+            <Bookmarks id={hike.id} />
             {/* Photo */}
             <AspectRatio ratio="1">
               <img src={hike.pictures[0]} loading="lazy" alt="" />
@@ -47,69 +47,67 @@ export default function CardComponent(hike: Hike) {
             </div>
           </CardContent>
 
+          {/* Card content */}
+          <CardContent>
+            <h3 className="card__title">{hike.title}</h3>
+            <Typography level="body-sm">{cutText(hike.description)}</Typography>
+          </CardContent>
 
-        {/* Card content */}
-        <CardContent>
-          <h3 className="card__title">{hike.title}</h3>
-          <Typography level="body-sm">{cutText(hike.description)}</Typography>
-        </CardContent>
-
-            {/* Card tags */}
-            <CardContent
-              orientation="horizontal"
-              className="card__tags"
-              sx={{
+          {/* Card tags */}
+          <CardContent
+            orientation="horizontal"
+            className="card__tags"
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              padding: '10px',
+              backgroundColor: '#f5f5f5',
+              borderRadius: '8px',
+              '& .column': {
                 display: 'flex',
-                justifyContent: 'space-between',
+                flexDirection: 'column',
                 alignItems: 'center',
-                padding: '10px',
-                backgroundColor: '#f5f5f5',
-                borderRadius: '8px',
-                '& .column': {
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
-                  textAlign: 'center',
-                  '& .MuiTypography-root': {
-                    fontSize: '0.8rem',
-                    color: '#555',
-                    whiteSpace: 'nowrap',
-                    overflow: 'hidden',
-                    textOverflow: 'ellipsis',
-                  },
+                textAlign: 'center',
+                '& .MuiTypography-root': {
+                  fontSize: '0.8rem',
+                  color: '#555',
+                  whiteSpace: 'nowrap',
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
                 },
-                '& .localisation-column': {
-                  flexBasis: '33%',
-                  textAlign: 'left',
-                },
-                '& .time-column': {
-                  flexBasis: '33%',
-                },
-                '& .difficulty-column': {
-                  flexBasis: '33%',
-                },
-              }}
-            >
-              <div className="column localisation-column">
-                <Typography level="body-sm">Localisation:</Typography>
-                <Typography level="body-sm">{hike.localisation}</Typography>
-              </div>
+              },
+              '& .localisation-column': {
+                flexBasis: '33%',
+                textAlign: 'left',
+              },
+              '& .time-column': {
+                flexBasis: '33%',
+              },
+              '& .difficulty-column': {
+                flexBasis: '33%',
+              },
+            }}
+          >
+            <div className="column localisation-column">
+              <Typography level="body-sm">Localisation:</Typography>
+              <Typography level="body-sm">{hike.localisation}</Typography>
+            </div>
 
-              <div className="column time-column">
-                <Typography level="body-sm">Marche:</Typography>
-                <Typography level="body-sm">
-                  {formatHikeTime(hike.time)}
-                </Typography>
-              </div>
+            <div className="column time-column">
+              <Typography level="body-sm">Marche:</Typography>
+              <Typography level="body-sm">
+                {formatHikeTime(hike.time)}
+              </Typography>
+            </div>
 
-              <div className="column difficulty-column">
-                <Typography level="body-sm">Difficulté:</Typography>
-                <div className="column other-column">
-                  {RenderDifficulty(hike)}
-                </div>
+            <div className="column difficulty-column">
+              <Typography level="body-sm">Difficulté:</Typography>
+              <div className="column other-column">
+                {RenderDifficulty(hike)}
               </div>
-            </CardContent>
-          </CardOverflow>
+            </div>
+          </CardContent>
         </Card>
       </NavLink>
     </>
