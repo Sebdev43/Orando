2b47bf45c94a7093e73dc7b51b33bb81b34af5c5
Sebdev43 <sebastien.robert@oclock.school>
2024-07-18 13:26:04 +0200

    feat: :white_check_mark: Update bookmarksValidator

--- app/validators/bookmarksValidators.js
+++ app/validators/bookmarksValidators.js
@@ -1,18 +1,25 @@
 import { body, validationResult } from 'express-validator';
+import { hikesDataMappers } from '../dataMappers/index.dataMappers.js';
 
 export const bookmarksValidators = [
     body('hikeId')
         .exists().withMessage('Le paramètre hikeId est requis.')
-        .isInt().withMessage('Le paramètre hikeId doit être un entier.'),
-        (req, res, next) => {
-            const errors = validationResult(req);
-            if (!errors.isEmpty()) {
-                return res.status(400).json({
-                    status: "error",
-                    message: "Données de requête invalides",
-                    errors: errors.array(),
-                });
+        .isInt().withMessage('Le paramètre hikeId doit être un entier.')
+        .custom(async hikeId => {
+            const hike = await hikesDataMappers.getHikeById(hikeId);
+            if (!hike) {
+                return Promise.reject('Aucun hike trouvé avec cet ID.');
             }
-            next();
+        }),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({
+                status: "error",
+                message: "Données de requête invalides",
+                errors: errors.array(),
+            });
         }
+        next();
+    }
 ];
